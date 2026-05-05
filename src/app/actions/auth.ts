1b414ccb8e8535bdb3aafcd102fcacdbcd7804ad
gmlduqzhd <gmlduqzhd123@gmail.com>
2026-05-05 15:40:59 +0900

    Redirect to login after logout

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -2,6 +2,7 @@
 
 import { createClient } from '@/utils/supabase/server'
 import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 
 export async function login(data: { name: string; password: string }) {
   const supabase = await createClient()
@@ -60,4 +61,5 @@ export async function logout() {
   const supabase = await createClient()
   await supabase.auth.signOut()
   revalidatePath('/', 'layout')
+  redirect('/login')
 }
